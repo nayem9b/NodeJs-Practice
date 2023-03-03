9a42f44ea705da140c3bf7c15a23779659873d19
nayem9b <razwanniam1@gmail.com>
2023-03-03 16:59:24 +0600

    array with resolved case

--- 2.promise.js
+++ 2.promise.js
@@ -1,2 +1,14 @@
 const userIds = [1, 2, 3, 4, 5];
-console.log(userIds);
+const myPromise = new Promise((resolved, rejected) => {
+  console.log("hello");
+});
+let userData = [];
+
+for (let i = 0; i < userIds.length; i++) {
+  const userId = userIds(i);
+  userData.push(myPromise);
+}
+
+Promise.all(userData).then((res) => {
+  console.log(res);
+});
