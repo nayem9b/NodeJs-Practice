4d990f52455de49bb641f60d34f608b49f123904
nayem9b <razwanniam1@gmail.com>
2023-03-06 04:34:12 +0600

    fs module system

--- 4.server.js
+++ 4.server.js
@@ -1,25 +1,41 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-  const address_url =
-    "http://localhost:5000/dashboard?name=nayem&country=bangladesh";
-  const parsed_url = url.parse(address_url, true);
-  console.log(parsed_url);
-  const testJson = {
-    name: "Kashem",
-    age: 25,
-  };
+  //   const address_url =
+  //     "http://localhost:5000/dashboard?name=nayem&country=bangladesh";
+  //   const parsed_url = url.parse(address_url, true);
+  //   const queryObject = parsed_url.query;
+  //   console.log(queryObject);
+  //   console.log(parsed_url);
+  //   const testJson = {
+  //     name: "Kashem",
+  //     age: 25,
+  //   };
+  if ((req.url = "/diff")) {
+    fs.readFile("data.text", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.write("Failed to read data");
+        res.end();
+      } else {
+        res.write(data);
+        res.end();
+      }
+    });
+  }
 
   //   res.end("Hello from node.js server");
-  if ((req.url = "/")) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`<p> You are getting this from homepage</p>`);
-    res.end();
-  } else if ((req.url = "/contact")) {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(testJson));
-    res.end();
-  }
+  //   if ((req.url = "/")) {
+  //     res.writeHead(200, { "Content-Type": "text/html" });
+  //     res.write(`<p> You are getting this from homepage</p>`);
+  //     res.end();
+  //   } else if ((req.url = "/contact")) {
+  //     res.writeHead(200, { "Content-Type": "application/json" });
+  //     res.write(JSON.stringify(testJson));
+  //     res.end();
+  //   }
 });
 const PORT = 5000;
 server.listen(PORT);
