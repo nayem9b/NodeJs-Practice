8c8ea5862ea62e2f8cb3edd883d9a91ac316d845
nayem9b <razwanniam1@gmail.com>
2023-03-06 03:24:28 +0600

    nodemon

--- 4.server.js
+++ 4.server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.end("Hello from node.js server");
+  console.log(req.url);
 });
 const PORT = 5000;
 server.listen(PORT);
