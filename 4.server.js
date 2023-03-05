025a269053666629034e07c5e434ea47e7020f16
nayem9b <razwanniam1@gmail.com>
2023-03-06 04:08:35 +0600

    address url parse

--- 4.server.js
+++ 4.server.js
@@ -1,5 +1,10 @@
 const http = require("http");
+const url = require("url");
 const server = http.createServer((req, res) => {
+  const address_url =
+    "http://localhost:5000/dashboard?name=nayem&country=bangladesh";
+  const parsed_url = url.parse(address_url, true);
+  console.log(parsed_url);
   const testJson = {
     name: "Kashem",
     age: 25,
@@ -15,11 +20,6 @@ const server = http.createServer((req, res) => {
     res.write(JSON.stringify(testJson));
     res.end();
   }
-  //   if ((req.url = "/")) {
-  //     res.writeHead(200, { "Content-Type": "text/html" });
-  //     res.write(`<p> You are getting this from homepage</p>`);
-  //     res.end();
-  //   }
 });
 const PORT = 5000;
 server.listen(PORT);
