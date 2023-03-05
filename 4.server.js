2277519932c8cddb76c05e3269fad497f60e0836
nayem9b <razwanniam1@gmail.com>
2023-03-06 03:47:41 +0600

    with writehead

--- 4.server.js
+++ 4.server.js
@@ -1,7 +1,11 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("Hello from node.js server");
-  console.log(req.url);
+  //   res.end("Hello from node.js server");
+  if ((req.url = "/")) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<p> You are getting this from homepage</p>`);
+    res.end();
+  }
 });
 const PORT = 5000;
 server.listen(PORT);
